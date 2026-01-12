4c03cdc651dd43f64936737a373ebbd39c383fa5
Shajjad002 <shajjd002@gmail.com>
2026-01-12 17:24:20 +0600

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/components/ui/button";
 
-const Page = () => {
-  
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+
+const Page = async () => {
+ const users = await prisma.user.findMany()
+ 
   return( 
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button variant="outline" >
-        Click Me
-      </Button>
+       {JSON.stringify(users)}
     </div>
   );
 };
---
+++ src/lib/db.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@/generated/prisma"
+import { PrismaPg } from "@prisma/adapter-pg"
+import pg from "pg"
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL!,
+})
+
+const adapter = new PrismaPg(pool)
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter, // ✅ REQUIRED in Prisma 7
+  })
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
+
+export default prisma
